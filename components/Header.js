2c3e1ce9aa633271f3345e63520c174cf2d3b4b8
Tad-z <david.akintade1000@gmail.com>
2023-10-12 14:45:01 +0100

    metaData

--- components/Header.js
+++ components/Header.js
@@ -62,7 +62,7 @@ const Header = ({ title }) => {
       }
     };
 
-    const handleClick = async() => {
+    const handleClick = async () => {
       await getCartItems()
     }
 
@@ -96,9 +96,16 @@ const Header = ({ title }) => {
   return (
     <div>
       <Head>
-        <title>{title ? title + "-EcomerceApp" : "EcommerceApp"}</title>
+        <title>{title ? title + "-EMAX" : "EMAX"}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Ecommerce website, sells clothes,shoes,jeans,hoodies" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="icon" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <ToastContainer position="bottom-center" limit={1} autoClose={3000} />
