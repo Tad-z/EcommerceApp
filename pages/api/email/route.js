dd1f6ac130bcb94451f55df80a3cd477235bf7d3
Tad-z <david.akintade1000@gmail.com>
2023-11-06 15:04:16 +0100

    setting up order-confirmation-mail

--- pages/api/email/route.js
+++ pages/api/email/route.js
@@ -1,9 +1,11 @@
 import nodemailer from 'nodemailer';
+import path from 'path'
+import hbs from 'nodemailer-express-handlebars';
 
 export default async function POST(req, res) {
   try{
-
-  // const { email, message } = await req.json();
+  const { email, message } = await req.body;
+  console.log(email);
   console.log(process.env.MY_EMAIL);
 
   const transport = nodemailer.createTransport({
@@ -14,11 +16,25 @@ export default async function POST(req, res) {
     },
   });
 
+  const handlebarOptions = {
+    viewEngine: {
+        partialsDir: process.cwd() + "/pages/email_templates/",
+        defaultLayout: false,
+    },
+    viewPath: process.cwd() + "/pages/email_templates/",
+};
+
+transport.use('compile', hbs(handlebarOptions))
+
   const mailOptions = {
     from: process.env.MY_EMAIL,
+    template: "order_confirmation",
     to: "david.akintade1000@gmail.com",
     subject: `Order Confirmation`,
-    text: "Hello world",
+    context: {
+      name: "tade",
+      company: 'emax'
+    },
   };
 
     const sendMail = await transport.sendMail(mailOptions)
@@ -29,9 +45,22 @@ export default async function POST(req, res) {
       console.log("Email not sent")
       return res.status(400).json({ message: "Email not sent" })
     }
+
+    // await new Promise((resolve, reject) => {
+    //   // send mail
+    //   transporter.sendMail(mailOptions, (err, response) => {
+    //     if (err) {
+    //       reject(err);
+    //     } else {
+    //       resolve(response);
+    //     }
+    //   });
+    // });
+    // no try catch
     
 
   } catch(error) {
+    console.log(error)
     return res.status(500).json({ error }, { status: 500 })
   }
  
