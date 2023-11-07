98d2bb4a97ef48290773698a1c9d322fa2d76367
Tad-z <david.akintade1000@gmail.com>
2023-11-07 17:00:24 +0100

    email-fix

--- pages/api/helper.js
+++ pages/api/helper.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import nodemailer from 'nodemailer';
-import hbs from 'nodemailer-express-handlebars';
 
 export async function getServerData(url) {
   if (typeof window !== "undefined") {
@@ -55,52 +53,6 @@ export default class ApiCall {
   }
 }
 
-
-const transport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.MY_EMAIL,
-    pass: process.env.MY_PASSWORD,
-  },
-});
-
-const handlebarOptions = {
-  viewEngine: {
-    partialsDir: process.cwd() + '/pages/email_templates/',
-    defaultLayout: false,
-  },
-  viewPath: process.cwd() + '/pages/email_templates/',
-};
-
-transport.use('compile', hbs(handlebarOptions));
-
-export async function sendOrderConfirmationEmail(orderData) {
-  const { fullname, phoneNumber, city, adress,  user, cartItems } = orderData;
-
-  const mailOptions = {
-    from: process.env.MY_EMAIL,
-    template: 'order_confirmation',
-    to: user.email, // Send the email to the user's email address
-    subject: 'Order Confirmation',
-    context: {
-      name: user.username,
-      orderItems: cartItems,
-      fullname,
-      phoneNumber,
-      city,
-      adress,
-    },
-  };
-
-  try {
-    const sendMail = await transport.sendMail(mailOptions);
-    console.log('Order Confirmation Email sent successfully');
-    return sendMail;
-  } catch (error) {
-    console.error('Error sending Order Confirmation Email:', error);
-    throw error;
-  }
-}
 
 
 
